f0dcfe8fca0a8e6b757c7bac70217efd93472780
setbap <ebr.sina@outlook.com>
2023-01-06 06:48:07 +0330

    add defi menu

--- src/lib/utility/sideMenuItems.ts
+++ src/lib/utility/sideMenuItems.ts
@@ -1,11 +1,13 @@
 import { GiMeepleCircle, GiProfit } from "react-icons/gi";
 import { BsCodeSquare, BsFileEarmarkImage } from "react-icons/bs";
 import { BiTransferAlt } from "react-icons/bi";
+import { AiOutlineSwap } from "react-icons/ai";
 
 export default [
   { name: "Transactions", path: "/", icon: BiTransferAlt },
   { name: "Staking", path: "/staking", icon: GiProfit },
   { name: "Supply", path: "/supply", icon: GiMeepleCircle },
+  { name: "Defi", path: "/defi", icon: AiOutlineSwap },
   { name: "NFT", path: "/nft", icon: BsFileEarmarkImage },
   { name: "Development", path: "/development", icon: BsCodeSquare },
 ];
